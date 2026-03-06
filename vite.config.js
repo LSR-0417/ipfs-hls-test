e73508a7fb01d9832bcd85a2475568b1fa9155b3
iskku@MBP M1 Pro <Insect.LSR@gmail.com>
2026-03-07 04:47:20 +0800

    開放外部監聽

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,9 @@ import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
+  server: {
+    host: true, // Listen on all local IPs
+  },
   plugins: [vue()],
   resolve: {
     alias: {
